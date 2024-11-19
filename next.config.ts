e3a264ab9db9380a4a57a5811c369bcfafabf6c8
Alex Luo <zz098708150829@gmail.com>
2024-11-19 22:02:44 +0800

    feat: next config指定env參數

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_CHAT_ID: process.env.NEXT_PUBLIC_CHAT_ID,
+  },
 };
 
 export default nextConfig;
